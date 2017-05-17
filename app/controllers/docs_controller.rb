643220659cc1d15db6ab4017e545583a573a236b
Tim DiSabatino <tim@pathwaysdigital.com>
2017-05-17 15:12:59 -0700

    Add docs controller, form partial with fields and form params

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -7,9 +7,17 @@ class DocsController < ApplicationController
   end
 
   def new
+    @doc = Doc.new
   end
 
   def create
+    @doc = Doc.new(doc_params)
+
+    if @doc.save
+      redirect_to @doc
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -27,5 +35,6 @@ class DocsController < ApplicationController
     end
 
     def doc_params
+      params.require(:doc).permit(:title, :content)
     end
 end
